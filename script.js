05ebf96af25f7d29447207fb3a69cc3e5a9dbc8d
Dallan Dunn <dallanpdunn@gmail.com>
2022-01-15 19:19:43 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -3,4 +3,31 @@ function computerPlay () {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return `It is a Tie! You both chose ${computerSelection}`;
+    }
+    switch (playerSelection.toLowerCase()) {
+        case 'rock':
+            if (computerSelection === 'Paper') {
+                return 'You Lose! Paper beats Rock';
+            } else {
+                return 'You Win! Rock beats Scissors';
+            }
+
+        case 'paper':
+            if (computerSelection === 'Rock') {
+                return 'You Win! Paper beats Rock';
+            } else {
+                return 'You Lose! Scissors beats Paper';
+            }
+        
+        case 'scissors':
+            if (computerSelection === 'Paper') {
+                return 'You Win! Scissors beats Paper';
+            } else {
+                return 'You Lose! Rock beats Scissors';
+            }
+    }
+}
+
