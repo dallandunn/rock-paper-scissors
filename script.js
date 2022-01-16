c0a7b21cbebfcda9b01ff28e1443ae1ae72cf5ef
Dallan Dunn <dallanpdunn@gmail.com>
2022-01-15 20:06:17 -0500

    Add function to play game

--- script.js
+++ script.js
@@ -5,29 +5,65 @@ function computerPlay () {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return `It is a Tie! You both chose ${computerSelection}`;
+        console.log(`It is a Tie! You both chose ${computerSelection}`);
+        return 'no winner'
     }
     switch (playerSelection.toLowerCase()) {
         case 'rock':
             if (computerSelection === 'Paper') {
-                return 'You Lose! Paper beats Rock';
+                console.log('You Lose! Paper beats Rock')
+                return 'computer';
             } else {
-                return 'You Win! Rock beats Scissors';
+                console.log('You Win! Rock beats Scissors');
+                return 'player';
             }
 
         case 'paper':
             if (computerSelection === 'Rock') {
-                return 'You Win! Paper beats Rock';
+                console.log('You Win! Paper beats Rock');
+                return 'player';
             } else {
-                return 'You Lose! Scissors beats Paper';
+                console.log('You Lose! Scissors beats Paper');
+                return 'computer';
             }
         
         case 'scissors':
             if (computerSelection === 'Paper') {
-                return 'You Win! Scissors beats Paper';
+                console.log('You Win! Scissors beats Paper');
+                return 'player';
             } else {
-                return 'You Lose! Rock beats Scissors';
+                console.log('You Lose! Rock beats Scissors');
+                return 'computer';
             }
     }
 }
 
+function game() {
+    let playerWins = 0;
+    let computerWins =0;
+
+    for (let i = 1; i < 6; i++) {
+        console.log(`Round ${i}:`);
+        let playerSelection = prompt('Rock, Paper or Scissors?');
+        let computerSelection = computerPlay();
+        let winner = playRound(playerSelection, computerSelection);
+
+        if (winner === 'player') {
+            playerWins++;
+        }
+
+        if (winner === 'computer') {
+            computerWins++;
+        }
+    }
+
+    if (computerWins > playerWins) {
+        console.log(`You Lose! The computer won ${computerWins} Rounds. You won ${playerWins} rounds.`);
+    } else if (playerWins > computerWins) {
+        console.log(`You win! The computer won ${computerWins} Rounds. You won ${playerWins} rounds.`);
+    } else {
+        console.log(`It is a tie! The computer won ${computerWins} Rounds. You won ${playerWins} rounds.`)
+    }
+}
+
+game()
